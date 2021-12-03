864be33bdf75522c7adf7500ff29ba4cca724ba7
Henry Odza <64870011+Hwodza@users.noreply.github.com>
2021-12-02 20:15:26 -0500

    commit calfflac.java

--- calfflac.java
+++ calfflac.java
@@ -1,1 +1,27 @@
+/*
+ID: hwodza1
+TASK: calfflac
+LANG: JAVA
+*/
+import java.io.*;
+import java.util.*;
+class Main {
+	public static ArrayList<Character> let = new ArrayList<Character>();
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new FileReader("calfflac.in"));
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("calfflac.out")));
+		String str = "";
+		while((str = in.readLine()) != null)
+		{
+			for(int i=0; i<str.length(); i++)
+			{
+				let.add(str.charAt(i));
+			}
+		}
 
+		
+		in.close();
+		out.close();
+		System.exit(0);
+	}
+}
