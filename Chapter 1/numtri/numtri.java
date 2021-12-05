c81c2976e06b4e8693fdd6981df8c8a60ac0d0ee
Henry Odza <64870011+Hwodza@users.noreply.github.com>
2021-12-04 20:28:32 -0500

    commit numtri.java

--- Chapter 1/numtri/numtri.java
+++ Chapter 1/numtri/numtri.java
@@ -18,7 +18,8 @@ public class numtri
         //Creates input and output files
         BufferedReader in = new BufferedReader(new FileReader("numtri.in"));
         PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("numtri.out")));
-
+        
+        //Reads in input data
         int numRows = Integer.parseInt(in.readLine());
         int[][] triangle = new int[numRows+1][numRows+1];
         int[][] output = new int[numRows+1][numRows+1];
@@ -32,6 +33,7 @@ public class numtri
             }
         }
         
+        //Calls recursive function and prints result
         out.println(search(numRows, triangle, output, 0, 0));
         in.close();
         out.close();
@@ -39,18 +41,22 @@ public class numtri
 
     public static int search(int numRows, int triangle[][], int output[][], int i, int j)
     {
+        //Base Case
         if(output[i][j] != -1)
         {
             return output[i][j];
         }else if(i <= triangle.length){
+            //Find the two neighboring points
             int num1 = search(numRows, triangle, output, i+1, j);
             int num2 = search(numRows, triangle, output, i+1, j+1);
+            //Determines which one is bigger for sum
             if(num1 > num2)
             {
                 output[i][j] = num1 + triangle[i][j];
             }else{
                 output[i][j] = num2 + triangle[i][j];
             }
+            //Returns sum
             return output[i][j];
         }else{
             return 0;
