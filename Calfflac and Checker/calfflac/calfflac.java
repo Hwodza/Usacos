9ad1ada7dda33c229a47679a434db2b45c6b05a9
Henry Odza <64870011+Hwodza@users.noreply.github.com>
2021-12-07 09:23:58 -0500

    commit calfflac.java

--- Calfflac and Checker/calfflac/calfflac.java
+++ Calfflac and Checker/calfflac/calfflac.java
@@ -7,12 +7,15 @@ import java.io.*;
 import java.util.*;
 class Main {
 	public static ArrayList<Character> let = new ArrayList<Character>();
+	public static ArrayList<Integer> index = new ArrayList<Integer>();
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new FileReader("calfflac.in"));
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("calfflac.out")));
 		String str = "";
 		while((str = in.readLine()) != null)
 		{
+			index.add(str.length());
 			for(int i=0; i<str.length(); i++)
 			{
 				let.add(str.charAt(i));
