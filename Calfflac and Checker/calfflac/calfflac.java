bc99809d51e3f3e70b6980d38b19c2493a63721a
Henry <hwodza@gmail.com>
2021-12-08 12:45:27 -0500

    commit calfflac.java

--- Calfflac and Checker/calfflac/calfflac.java
+++ Calfflac and Checker/calfflac/calfflac.java
@@ -7,8 +7,10 @@ import java.io.*;
 import java.util.*;
 class Main {
 	public static ArrayList<Character> let = new ArrayList<Character>();
+	public static ArrayList<Character> let2 = new ArrayList<Character>();
 	public static ArrayList<Integer> index = new ArrayList<Integer>();
-	
+	public static ArrayList<Boolean> equal = new ArrayList<Boolean>();
+	public static ArrayList<Boolean> good = new ArrayList<Boolean>();
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new FileReader("calfflac.in"));
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("calfflac.out")));
@@ -19,8 +21,24 @@ class Main {
 			for(int i=0; i<str.length(); i++)
 			{
 				let.add(str.charAt(i));
+				String temp = "" + str.charAt(i);
+				temp = temp.toLowerCase();
+				char temp1 = temp.charAt(0);
+				let2.add(temp.charAt(0));
+				if(temp1 >= 97 && temp1 <= 122)
+				{
+					good.add(true);
+				}else{
+					good.add(false);
+				}
 			}
 		}
+		int x = 0, y = 1;
+		while(x < let.size()-2)
+		{
+			x++;
+		}
+
 
 		out.print("Hello");
 		in.close();
