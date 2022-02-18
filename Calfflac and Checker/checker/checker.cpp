508f11a9929de3c44ad573196dbfb34eab15ff39
Henry Odza <64870011+Hwodza@users.noreply.github.com>
2022-02-18 14:15:41 -0500

    commit all

--- Calfflac and Checker/checker/checker.cpp
+++ Calfflac and Checker/checker/checker.cpp
@@ -25,28 +25,37 @@ using namespace std;
 
 ofstream fout ("checker.out");
 //Global Variables
-int tempn = 0;
-int *pn = &tempn;
+int n;
 bool rows[13] = {0}; //False = empty, True = full
 int placement[13]; 
 bool** diags = new bool*[2];
+int total{0};
+int printed{0};
 
 void placequeen(int column){
-    if(column == *pn){
-        for(int i=0; i<*pn; i++){
-            cout << placement[i] << " ";
+    if(column == n){
+        if(printed > 2)
+        {
+            ++total;
+            return;
         }
-        cout << endl;
+        ++total;
+        ++printed;
+        fout << placement[0];
+        for(int i=1; i<n; ++i){
+            fout << " " << placement[i];
+        }
+        fout << endl;
         return;
     }
-    for(int row=0; row<*pn; row++){
+    for(int row=0; row<n; ++row){
         //Do something
         
         if(!rows[row]&&!diags[0][-1*row+column+5]&&!diags[1][row+column]){
             rows[row] = true;
             diags[0][-1*row+column + 5] = true;
             diags[1][row+column] = true;
-            placement[row] = column+1;
+            placement[column] = row+1;
             placequeen(column+1);
             rows[row] = false;
             diags[0][-1*row+column + 5] = false;
@@ -59,60 +68,13 @@ void placequeen(int column){
 int main(){
     
     ifstream fin ("checker.in");
-    int n; fin >> n;
-    *pn = n;
+    fin >> n;
     diags[0] = new bool[n*2-1]; //Top left to bottom right diagonal check
     diags[1] = new bool[n*2-1]; //Top right to bottom left diagonal check
     memset(diags[0], false, sizeof(diags[0]));
     memset(diags[1], false, sizeof(diags[1]));
     placequeen(0);
-    cout << *pn << "\n";
+    fout << total << '\n';
     return 0;
 }
 
-/*
-bool** outer = new bool*[3];
-int** ptorder = new int*[1];
-int temp = 0;
-int* p = &temp;
-
-void placequeen(int column){
-    
-    if(column == *p){
-        for(int i=0; i<*p; i++){
-            fout << ptorder[0][i]+1 << " ";
-        }
-        return;
-    }
-    for(int row = 0; row < *p; row++){
-        if(outer[0][row] && outer[1][-1*row+column + 5] && outer[2][row+column]){
-            ptorder[0][row] = column;
-            outer[0][row] = false;
-            outer[1][-1*row+column + 5] = false;
-            outer[2][row+column] = false;
-            placequeen(column + 1);
-            outer[0][row] = true;
-            outer[1][-1*row+column + 5] = true;
-            outer[2][row+column] = true;
-        }
-    }
-    
-}
-
-
-int main(){
-    
-    ifstream fin ("checker.in");
-    int n; cin >> n;
-    
-    *p = n;
-    outer[0] = new bool[n]; //Row check
-    outer[1] = new bool[n*2-1]; //Top left to bottom right diagonal check
-    outer[2] = new bool[n*2-1]; //Top right to bottom left diagonal check
-    memset(outer[0], true, sizeof(outer[0]));
-    memset(outer[1], true, sizeof(outer[1]));
-    memset(outer[2], true, sizeof(outer[2]));
-    ptorder[0] = new int[n];
-    placequeen(0);
-    return 0;
-}*/
