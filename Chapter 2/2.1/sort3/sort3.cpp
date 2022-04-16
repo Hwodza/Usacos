e5a8f2460dc918ef6d84c80f335394482655b29e
Henry Odza <64870011+Hwodza@users.noreply.github.com>
2022-04-16 17:37:07 -0400

    commit

--- Chapter 2/2.1/sort3/sort3.cpp
+++ Chapter 2/2.1/sort3/sort3.cpp
@@ -27,6 +27,57 @@ int main(){
     //IO
     ofstream fout ("sort3.out");
     ifstream fin ("sort3.in");
+
+    //Reads in data
     int n;
-    cout << "Hello\n";
+    fin >> n;
+    int original[n];
+    int sorted[n];
+    for(int i=0; i<n; ++i)
+    {
+        int temp;
+        fin >> temp;
+        original[i] = temp;
+        sorted[i] = temp;
+    }
+
+    //Sorts the sorted list
+    for(int i=0; i<n-1; ++i)
+    {
+        for(int j=0; j<n-i-1; ++j)
+        {
+            if(sorted[j] > sorted[j+1])
+            {
+                int temp = sorted[j];
+                sorted[j] = sorted[j+1];
+                sorted[j+1] = temp;
+            }
+        }
+    }
+    
+    //Finds all of the one move swaps
+    int swaps = 0;
+    for(int i=0; i<n; ++i)
+    {
+        for(int j=0; j<n; ++j)
+        {
+            if(original[i] != sorted[i] && original[j] != sorted[j] && original[i] == sorted[j] && original[j] == sorted[i])
+            {
+                original[i] = sorted[i];
+                original[j] = sorted[j];
+                ++swaps;
+            }
+        }
+    }
+    //Finds all differences
+    int numdiff = 0;
+    for(int i=0; i<n; ++i)
+    {
+        if(original[i] != sorted[i])
+        {
+            numdiff++;
+        }
+    }
+    //Outputs (numdiff/3*2 is done because the rest are in groups of 3 that need two swaps)
+    fout << numdiff/3*2 + swaps << endl;
 }
